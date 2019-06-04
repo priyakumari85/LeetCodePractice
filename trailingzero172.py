a25c842d11393439157f6139fff5b676c7f0e391
priyakumari85 <priya_kumari85@outlook.com>
2019-06-03 22:30:24 -0500

    Adding solution to #172

---
+++ trailingzero172.py
@@ -0,0 +1,18 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        #counter = 0
+        #out = 0
+        #if 5**counter<=n:
+        #    out = out + (n/5**counter)
+        #    counter+=1
+        #return int(out)
+
+        res = 0
+        while n > 0:
+            n = n // 5
+            res += n
+        return res
+
+
+
+    
